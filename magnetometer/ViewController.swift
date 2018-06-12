3dc09b78c460f1efc6f9915d338275389560d3a1
Jonah Majumder <jmajumder9@gmail.com>
2018-06-11 21:41:15 -0400

    Initial Commit

---
+++ magnetometer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  magnetometer
+//
+//  Created by Jonah Majumder on 6/11/18.
+//  Copyright © 2018 Jonah Majumder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
