8add3178ef85b0680d4c03cdf03b1072c4114e18
Jonah Majumder <jmajumder9@gmail.com>
2018-07-15 19:16:59 -0400

    magnetometer ui update working

--- magnetometer/ViewController.swift
+++ magnetometer/ViewController.swift
@@ -7,19 +7,80 @@
 //
 
 import UIKit
+import CoreMotion
+
+func BG(_ block: @escaping ()->Void) {
+    DispatchQueue.global(qos: .default).async(execute: block)
+}
+
+func UI(_ block: @escaping ()->Void) {
+    DispatchQueue.main.async(execute: block)
+}
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var BnetValue: UILabel!
+    @IBOutlet weak var BzValue: UILabel!
+    @IBOutlet weak var ByValue: UILabel!
+    @IBOutlet weak var BxValue: UILabel!
+    
+    let motionManager = CMMotionManager()
+    let refFrame : CMAttitudeReferenceFrame = CMAttitudeReferenceFrame()
+    
+    let Fs : Double = 10.0
+    
+    private func updateMagData() {
+        let _:CMMagnetometerData!
+        let _:Error!
+        
+        self.BnetValue.text = "hi"
+        
+        if motionManager.isDeviceMotionAvailable {
+            
+            motionManager.deviceMotionUpdateInterval = 1.0 / Fs
+            motionManager.showsDeviceMovementDisplay = true
+            print(String(format: "Fs set to %.2f Hz.", Fs))
+            
+            motionManager.startDeviceMotionUpdates(using: CMAttitudeReferenceFrame.xArbitraryCorrectedZVertical, to: OperationQueue(), withHandler: {
+                (motion, error) in
+                
+                if error != nil {
+                    print("error=\(error)")
+                }
+                
+                let BxData = motion?.magneticField.field.x ?? 0.0
+                let ByData = motion?.magneticField.field.y ?? 0.0
+                let BzData = motion?.magneticField.field.z ?? 0.0
+                let BnetData = sqrt(BxData*BxData + ByData*ByData + BzData*BzData)
+                
+                let BxStr : String? = String(format: "%.3f", BxData)
+                let ByStr : String? = String(format: "%.3f", ByData)
+                let BzStr : String? = String(format: "%.3f", BzData)
+                let BnetStr : String? = String(format: "%.3f", BnetData)
+                
+                print(BxStr ?? "nodata")
+                print(ByStr ?? "nodata")
+                print(BzStr ?? "nodata")
+                print(BnetStr ?? "nodata")
+                
+                UI() {
+                self.BxValue.text = BxStr
+                self.ByValue.text = ByStr
+                self.BzValue.text = BzStr
+                self.BnetValue.text = BnetStr
+                }
+                
+            })
+        }
+//        motionManager.stopDeviceMotionUpdates() // does this fire immediately?
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.updateMagData()
+        print("Mag data fcn returned.")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
