b789d86d4e58f8158da2879384af5cd0869401c2
Jonah Majumder <jmajumder9@gmail.com>
2018-12-12 20:57:35 -0500

    time chart working to plot live data

---
+++ Pods/Target Support Files/Pods-magnetometerTests/Pods-magnetometerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_magnetometerTests : NSObject
+@end
+@implementation PodsDummy_Pods_magnetometerTests
+@end
--- magnetometer/ViewController.swift
+++ magnetometer/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreMotion
+import Charts
 
 func BG(_ block: @escaping ()->Void) {
     DispatchQueue.global(qos: .default).async(execute: block)
@@ -18,6 +19,9 @@ func UI(_ block: @escaping ()->Void) {
 }
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var timePlotView: LineChartView!
+    
     @IBOutlet weak var BnetValue: UILabel!
     @IBOutlet weak var BzValue: UILabel!
     @IBOutlet weak var ByValue: UILabel!
@@ -26,22 +30,55 @@ class ViewController: UIViewController {
     let motionManager = CMMotionManager()
     let refFrame : CMAttitudeReferenceFrame = CMAttitudeReferenceFrame()
     
-    let Fs : Double = 10.0
+    let Fs : Double = 1.0
     
-    private func updateMagData() {
+    private var BxChartEntryArray: [ChartDataEntry] = []
+    private var ByChartEntryArray: [ChartDataEntry] = []
+    private var BzChartEntryArray: [ChartDataEntry] = []
+    private var BnetChartEntryArray: [ChartDataEntry] = []
+    private var dataPts: Int = 0
+    
+    private func startMagUpdates() {
         let _:CMMagnetometerData!
         let _:Error!
         
+        let Ts : Double = 1.0 / Fs
+        var timeElapsed = Double()
+        
+        let BxSet : LineChartDataSet = LineChartDataSet(values: self.BxChartEntryArray, label: "Bx")
+        BxSet.drawCirclesEnabled = false
+        BxSet.setColor(UIColor.red)
+        BxSet.axisDependency = YAxis.AxisDependency.left
+        BxSet.drawValuesEnabled = false
+        
+        let BySet : LineChartDataSet = LineChartDataSet(values: self.ByChartEntryArray, label: "By")
+        BySet.drawCirclesEnabled = false
+        BySet.setColor(UIColor.green)
+        BySet.axisDependency = YAxis.AxisDependency.left
+        BySet.drawValuesEnabled = false
+        
+        let BzSet : LineChartDataSet = LineChartDataSet(values: self.BzChartEntryArray, label: "Bz")
+        BzSet.drawCirclesEnabled = false
+        BzSet.setColor(UIColor.yellow)
+        BzSet.axisDependency = YAxis.AxisDependency.left
+        BzSet.drawValuesEnabled = false
+        
+        let BnetSet : LineChartDataSet = LineChartDataSet(values: self.BnetChartEntryArray, label: "Bnet")
+        BnetSet.drawCirclesEnabled = false
+        BnetSet.setColor(UIColor.black)
+        BnetSet.axisDependency = YAxis.AxisDependency.right
+        BnetSet.drawValuesEnabled = false
+        
         if motionManager.isDeviceMotionAvailable {
             
-            motionManager.deviceMotionUpdateInterval = 1.0 / Fs
+            motionManager.deviceMotionUpdateInterval = Ts
             motionManager.showsDeviceMovementDisplay = true
             
             motionManager.startDeviceMotionUpdates(using: CMAttitudeReferenceFrame.xArbitraryCorrectedZVertical, to: OperationQueue(), withHandler: {
                 (motion, error) in
                 
                 if error != nil {
-                    print("error=\(error)")
+                    print("error=\(String(describing: error))")
                 }
                 
                 let BxData = motion?.magneticField.field.x ?? 0.0
@@ -49,16 +86,31 @@ class ViewController: UIViewController {
                 let BzData = motion?.magneticField.field.z ?? 0.0
                 let BnetData = sqrt(BxData*BxData + ByData*ByData + BzData*BzData)
                 
-                let BxStr : String? = String(format: "%.3f", BxData)
-                let ByStr : String? = String(format: "%.3f", ByData)
-                let BzStr : String? = String(format: "%.3f", BzData)
-                let BnetStr : String? = String(format: "%.3f", BnetData)
+                self.dataPts += 1
+                timeElapsed = Double(self.dataPts) * Ts
+                
+                // update data set arrays
+                BxSet.addEntry(ChartDataEntry(x: timeElapsed, y: BxData))
+                BySet.addEntry(ChartDataEntry(x: timeElapsed, y: ByData))
+                BzSet.addEntry(ChartDataEntry(x: timeElapsed, y: BzData))
+                BnetSet.addEntry(ChartDataEntry(x: timeElapsed, y: BnetData))
+                
+                var BdataSetArray : [LineChartDataSet] = [BxSet, BySet, BzSet, BnetSet]
+                
+                let BLineChartData = LineChartData(dataSets: BdataSetArray)
+                
+                let BxStr : String? = String(format: "%.3f uT", BxData)
+                let ByStr : String? = String(format: "%.3f uT", ByData)
+                let BzStr : String? = String(format: "%.3f uT", BzData)
+                let BnetStr : String? = String(format: "%.3f uT", BnetData)
                 
                 UI() {
                 self.BxValue.text = BxStr
                 self.ByValue.text = ByStr
                 self.BzValue.text = BzStr
                 self.BnetValue.text = BnetStr
+                    
+                self.timePlotView.data = BLineChartData
                 }
                 
             })
@@ -70,10 +122,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.updateMagData()
-        print("Mag data fcn returned.")
-        print(String(format: "Fs set to %.2f Hz.", Fs))
-
+        self.startMagUpdates()
     }
 
 }
