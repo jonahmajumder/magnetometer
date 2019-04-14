2a2ee8743657252af6921a6427de0e84fe2cc1e6
Jonah Majumder <jmajumder9@gmail.com>
2019-04-14 18:26:43 -0400

    working sith swift 4.<2 and xcode 9

--- Podfile
+++ Podfile
@@ -1,12 +1,13 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '12.0'
+
 
 target 'magnetometer' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for magnetometer
-  pod 'Charts' 
+  pod 'Charts', '3.1.1'
 
   target 'magnetometerTests' do
     inherit! :search_paths
--- magnetometer/ViewController.swift
+++ magnetometer/ViewController.swift
@@ -18,30 +18,35 @@ func UI(_ block: @escaping ()->Void) {
     DispatchQueue.main.async(execute: block)
 }
 
-//public func fft(x: [Double]) -> ([Double],[Double]) {
-//    let N = x.count
-//    let dN = Double(N)
+public func fft(_ x: [Double]) -> [Double] {
+    let N = x.count
+    let dN = Double(N)
 //    var Xre: [Double] = Array(repeating:0, count:N)
 //    var Xim: [Double] = Array(repeating:0, count:N)
-//
-//    var dk : Double = 0.0
-//    var dn : Double
-//    for k in 0..<N
-//    {
-//        Xre[k] = 0
-//        Xim[k] = 0
-//        let kfactor = (dk*2.0*Double.pi)/dN
-//        dn = 0.0
-//        for n in 0..<N {
-//            let q = kfactor*dn
-//            Xre[k] += x[n]*cos(q) // Real part of X[k]
-//            Xim[k] -= x[n]*sin(q) // Imag part of X[k]
-//            dn += 1
-//        }
-//        dk += 1.0
-//    }
-//    return (Xre, Xim)
-//}
+    var Xmag: [Double] = Array(repeating:0, count:N)
+    
+    var Xre : Double
+    var Xim : Double
+
+    var dk : Double = 0.0
+    var dn : Double
+    for k in 0..<N
+    {
+        Xre = 0
+        Xim = 0
+        let kfactor = (dk*2.0*Double.pi)/dN
+        dn = 0.0
+        for n in 0..<N {
+            let q = kfactor*dn
+            Xre += x[n]*cos(q) // Real part of X[k]
+            Xim -= x[n]*sin(q) // Imag part of X[k]
+            dn += 1.0
+        }
+        Xmag[k] = sqrt(Xre*Xre + Xim*Xim)
+        dk += 1.0
+    }
+    return Xmag
+}
 
 @IBDesignable
 class DesignableView: UIView {
@@ -97,25 +102,28 @@ extension UIView {
 class ViewController: UIViewController {
     
     @IBOutlet weak var timePlotView: LineChartView!
+    @IBOutlet weak var timeContainer: DesignableView!
+    @IBOutlet weak var updateContainer: DesignableView!
+    
+    @IBOutlet weak var freqPlotView: LineChartView!
+    
+    
+//    @IBOutlet var swiper: UISwipeGestureRecognizer!
     
     @IBOutlet weak var leftYlabel: UILabel!
     @IBOutlet weak var rightYlabel: UILabel!
-    
     
     @IBOutlet weak var BnetValue: UILabel!
     @IBOutlet weak var BzValue: UILabel!
     @IBOutlet weak var ByValue: UILabel!
     @IBOutlet weak var BxValue: UILabel!
     
-    let motionManager = CMMotionManager()
-    let refFrame : CMAttitudeReferenceFrame = CMAttitudeReferenceFrame()
+    @objc let motionManager = CMMotionManager()
+    @objc let refFrame : CMAttitudeReferenceFrame = CMAttitudeReferenceFrame()
     
-    let Fs : Double = 10.0
+    private var Fs : Double = 10.0
     
-    private var BxChartEntryArray: [ChartDataEntry] = []
-    private var ByChartEntryArray: [ChartDataEntry] = []
-    private var BzChartEntryArray: [ChartDataEntry] = []
-    private var BnetChartEntryArray: [ChartDataEntry] = []
+    @objc let EmptyChartEntryArray: [ChartDataEntry] = []
     private var dataPts: Int = 0
     
     private var BxSet : LineChartDataSet = LineChartDataSet()
@@ -123,43 +131,60 @@ class ViewController: UIViewController {
     private var BzSet : LineChartDataSet = LineChartDataSet()
     private var BnetSet : LineChartDataSet = LineChartDataSet()
     
+    private var BnetFFTSet : LineChartDataSet = LineChartDataSet()
+    
+    private var BnetDoubleArray : [Double] = []
+    
     private func startMagUpdates() {
         let _:CMMagnetometerData!
         let _:Error!
         
+//        self.swiper.addTarget(self, action: #selector(ViewController.swipeReset))
+//        self.swiper.direction = .left
+//        self.timeContainer.addGestureRecognizer(swiper)
+        
         self.leftYlabel.transform = CGAffineTransform(rotationAngle: -CGFloat.pi/2.0)
         self.rightYlabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi/2.0)
         
-        self.timePlotView.legend.drawInside = true
-        print(self.timePlotView.legend.orientation)
+        self.timePlotView.legend.drawInside = false
+        self.timePlotView.legend.form = Legend.Form.circle
+        self.timePlotView.xAxis.labelPosition = XAxis.LabelPosition.bottom
         
-        self.BxSet = LineChartDataSet(values: BxChartEntryArray, label: "Bx")
+        self.BxSet = LineChartDataSet(values: EmptyChartEntryArray, label: "Bx")
         BxSet.drawCirclesEnabled = false
         BxSet.setColor(UIColor.red)
         BxSet.axisDependency = YAxis.AxisDependency.left
         BxSet.drawValuesEnabled = false
         
-        self.BySet = LineChartDataSet(values: self.ByChartEntryArray, label: "By")
+        self.BySet = LineChartDataSet(values: EmptyChartEntryArray, label: "By")
         BySet.drawCirclesEnabled = false
         BySet.setColor(UIColor.green)
         BySet.axisDependency = YAxis.AxisDependency.left
         BySet.drawValuesEnabled = false
         
-        self.BzSet = LineChartDataSet(values: self.BzChartEntryArray, label: "Bz")
+        self.BzSet = LineChartDataSet(values: EmptyChartEntryArray, label: "Bz")
         BzSet.drawCirclesEnabled = false
         BzSet.setColor(UIColor.blue)
         BzSet.axisDependency = YAxis.AxisDependency.left
         BzSet.drawValuesEnabled = false
         
-        self.BnetSet = LineChartDataSet(values: self.BnetChartEntryArray, label: "Bnet")
+        self.BnetSet = LineChartDataSet(values: EmptyChartEntryArray, label: "Bnet")
         BnetSet.drawCirclesEnabled = false
         BnetSet.setColor(UIColor.black)
         BnetSet.axisDependency = YAxis.AxisDependency.right
         BnetSet.drawValuesEnabled = false
         
-        let Ts : Double = 1.0 / Fs
+        self.BnetFFTSet = LineChartDataSet(values: EmptyChartEntryArray, label: "Bnet FFT")
+        BnetFFTSet.drawCirclesEnabled = false
+        BnetFFTSet.setColor(UIColor.black)
+        BnetFFTSet.axisDependency = YAxis.AxisDependency.left
+        BnetFFTSet.drawValuesEnabled = false
+        
+        let Ts : Double = 1.0 / self.Fs
         var timeElapsed = Double()
         var xAxisMin : Double = 0.0
+        
+//        print(Array(0...0).map{Double($0)})
         
         if motionManager.isDeviceMotionAvailable {
             
@@ -178,9 +203,22 @@ class ViewController: UIViewController {
                 let BzData = motion?.magneticField.field.z ?? 0.0
                 let BnetData = sqrt(BxData*BxData + ByData*ByData + BzData*BzData)
                 
-                self.dataPts += 1
+                self.BnetDoubleArray.append(BnetData)
+                
+                let BnetAvg : Double = self.BnetDoubleArray.reduce(0, +) / Double(self.BnetDoubleArray.count)
+                
+                let fftmag = fft(self.BnetDoubleArray.map{$0 - BnetAvg})
+                let fftaxis = Array(0...self.dataPts).map{Double($0)*self.Fs/Double(self.dataPts+1)}
+                
+//                print(fftaxis)
+                
+//                print(self.BnetDoubleArray.count)
+//                print("---")
+//                print(fftmag.count)
+//                print(fftmag.count/2)
+                
                 timeElapsed = Double(self.dataPts) * Ts
-                xAxisMin = Double.maximum(Ts, timeElapsed-10)
+                xAxisMin = Double.maximum(Ts, timeElapsed-(Ts*100.0))
                 
                 // update data set arrays
                 _ = self.BxSet.addEntry(ChartDataEntry(x: timeElapsed, y: BxData))
@@ -191,10 +229,19 @@ class ViewController: UIViewController {
                 let BdataSetArray : [LineChartDataSet] = [self.BxSet, self.BySet, self.BzSet, self.BnetSet]
                 let BLineChartData = LineChartData(dataSets: BdataSetArray)
                 
+                self.BnetFFTSet.values = self.EmptyChartEntryArray
+                for i in 0..<fftmag.count/2+1 {
+                    _ = self.BnetFFTSet.addEntry(ChartDataEntry(x: fftaxis[i], y: fftmag[i]))
+                }
+                
+                let BnetFFTData = LineChartData(dataSet: self.BnetFFTSet)
+                
                 let BxStr : String? = String(format: "%.3f uT", BxData)
                 let ByStr : String? = String(format: "%.3f uT", ByData)
                 let BzStr : String? = String(format: "%.3f uT", BzData)
                 let BnetStr : String? = String(format: "%.3f uT", BnetData)
+                
+                self.dataPts += 1
                                 
                 UI() {
                     self.BxValue.text = BxStr
@@ -205,6 +252,7 @@ class ViewController: UIViewController {
                     self.timePlotView.data = BLineChartData
                     self.timePlotView.xAxis.axisMinimum = xAxisMin
                     
+                    self.freqPlotView.data = BnetFFTData
                 }
                 
             })
@@ -212,6 +260,21 @@ class ViewController: UIViewController {
 //        motionManager.stopDeviceMotionUpdates() // does this fire immediately?
         
     }
+    
+//    @objc func swipeReset() {
+//        self.resetData()
+//    }
+    
+    private func resetData() {
+        print("got reset signal")
+        self.BxSet.values = EmptyChartEntryArray
+        self.BySet.values = EmptyChartEntryArray
+        self.BzSet.values = EmptyChartEntryArray
+        self.BnetSet.values = EmptyChartEntryArray
+        
+        self.dataPts = 0
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
