4d03f9cf376381408bd505c26f5549495d01fb54
Jonah Majumder <jmajumder9@gmail.com>
2019-04-16 23:03:37 -0400

    some changes to clean up freq plot

--- magnetometer/MagConstants.swift
+++ magnetometer/MagConstants.swift
@@ -17,11 +17,40 @@ struct MagConstants {
     struct StringConstants {
         
 //        let strAttrs = [NSAttributedStringKey.baselineOffset : -10.0] as [NSAttributedStringKey : Any]
-        
-        
-        
-        
     }
     
 }
 
+struct Helper {
+    static func fft(_ x: [Double]) -> [Double] {
+        let N = x.count
+        let dN = Double(N)
+        //    var Xre: [Double] = Array(repeating:0, count:N)
+        //    var Xim: [Double] = Array(repeating:0, count:N)
+        var Xmag: [Double] = Array(repeating:0, count:N)
+        
+        var Xre : Double
+        var Xim : Double
+        
+        var dk : Double = 0.0
+        var dn : Double
+        for k in 0..<N
+        {
+            Xre = 0
+            Xim = 0
+            let kfactor = (dk*2.0*Double.pi)/dN
+            dn = 0.0
+            for n in 0..<N {
+                let q = kfactor*dn
+                Xre += x[n]*cos(q) // Real part of X[k]
+                Xim -= x[n]*sin(q) // Imag part of X[k]
+                dn += 1.0
+            }
+            Xmag[k] = sqrt(Xre*Xre + Xim*Xim)
+            dk += 1.0
+        }
+        return Xmag
+    }
+    
+    
+}
--- magnetometer/ViewController.swift
+++ magnetometer/ViewController.swift
@@ -18,36 +18,6 @@ func UI(_ block: @escaping ()->Void) {
     DispatchQueue.main.async(execute: block)
 }
 
-public func fft(_ x: [Double]) -> [Double] {
-    let N = x.count
-    let dN = Double(N)
-//    var Xre: [Double] = Array(repeating:0, count:N)
-//    var Xim: [Double] = Array(repeating:0, count:N)
-    var Xmag: [Double] = Array(repeating:0, count:N)
-    
-    var Xre : Double
-    var Xim : Double
-
-    var dk : Double = 0.0
-    var dn : Double
-    for k in 0..<N
-    {
-        Xre = 0
-        Xim = 0
-        let kfactor = (dk*2.0*Double.pi)/dN
-        dn = 0.0
-        for n in 0..<N {
-            let q = kfactor*dn
-            Xre += x[n]*cos(q) // Real part of X[k]
-            Xim -= x[n]*sin(q) // Imag part of X[k]
-            dn += 1.0
-        }
-        Xmag[k] = sqrt(Xre*Xre + Xim*Xim)
-        dk += 1.0
-    }
-    return Xmag
-}
-
 @IBDesignable
 class DesignableView: UIView {
 }
@@ -151,6 +121,9 @@ class ViewController: UIViewController {
         self.timePlotView.xAxis.labelPosition = XAxis.LabelPosition.bottom
         self.timePlotView.chartDescription?.enabled = false
         
+        self.freqPlotView.xAxis.labelPosition = XAxis.LabelPosition.bottom
+        self.freqPlotView.rightAxis.enabled = false
+        self.freqPlotView.leftAxis.axisMinimum = 0
         self.freqPlotView.chartDescription?.enabled = false
         
         self.BxSet = LineChartDataSet(values: EmptyChartEntryArray, label: "Bx")
@@ -208,7 +181,7 @@ class ViewController: UIViewController {
                 
                 let BnetAvg : Double = self.BnetDoubleArray.reduce(0, +) / Double(self.BnetDoubleArray.count)
                 
-                let fftmag = fft(self.BnetDoubleArray.map{$0 - BnetAvg})
+                let fftmag = Helper.fft(self.BnetDoubleArray.map{$0 - BnetAvg})
                 let fftaxis = Array(0...self.dataPts).map{Double($0) * MagConstants.DAQ.SampleFrequency / Double(self.dataPts+1)}
                 
 //                print(fftaxis)
@@ -254,6 +227,7 @@ class ViewController: UIViewController {
                     self.timePlotView.xAxis.axisMinimum = xAxisMin
                     
                     self.freqPlotView.data = BnetFFTData
+                    
                 }
                 
             })
